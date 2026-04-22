e654c926d634815e8783595b8d57b0bb7915f579
bretjhribar <cutcoedgecominterslice@gmail.com>
2026-04-22 12:33:41 -0500

    post delete section

--- factor-alpha-platform/_count.py
+++
@@ -1,5 +0,0 @@
-import sqlite3
-conn = sqlite3.connect("data/alphas.db")
-n = conn.execute("SELECT COUNT(*) FROM alphas WHERE archived=0 AND interval='4h' AND universe='BINANCE_TOP50'").fetchone()[0]
-print(n)
-conn.close()
--- factor-alpha-platform/test_error.py
+++
@@ -1,7 +0,0 @@
-import sys, os
-sys.path.insert(0, os.path.abspath('.'))
-import pandas as pd
-from compare_pipelines import run_walkforward_qp, load_gp_alphas, features, alpha_signals
-
-results = run_walkforward_qp(features, alpha_signals, fee_bps=0.0, train_bars=720, val_bars=360, reeval_interval=6, start_bar_override=8800)
-print(results.tail())
